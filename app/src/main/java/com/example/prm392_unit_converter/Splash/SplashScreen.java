e6eb4fae5e2f3c7de4d5ce69820f0f137461ea5a
longthangdang <longthang2000@gmail.com>
2023-02-28 11:38:12 +0700

    Add design for splash screen

---
+++ app/src/main/java/com/example/prm392_unit_converter/Splash/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.prm392_unit_converter.Splash;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.prm392_unit_converter.R;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
