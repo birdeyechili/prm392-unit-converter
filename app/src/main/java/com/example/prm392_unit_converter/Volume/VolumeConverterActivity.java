8c57bbc032e01685d539a12fbec4ec5c70d946f8
AnPQ <anphamquoc09@gmail.com>
2023-03-18 21:29:25 +0700

    update unit list from Volume

--- app/src/main/java/com/example/prm392_unit_converter/Volume/VolumeConverterActivity.java
+++ app/src/main/java/com/example/prm392_unit_converter/Volume/VolumeConverterActivity.java
@@ -30,15 +30,15 @@ public class VolumeConverterActivity extends ConverterActivity {
     @Override
     public List<Unit> getUnitList() {
         List<Unit> unitList = new ArrayList<>();
-        unitList.add(new Unit("m3","Cubic Meter","1.0"));
-        unitList.add(new Unit("km3","Cubic Kilometer","1.E-9"));
-        unitList.add(new Unit("cm3","Cubic Centimeter","1000000.0"));
-        unitList.add(new Unit("mm3","Cubic Milimeter","1000000000.0"));
+        unitList.add(new Unit("m^3","Cubic Meter","1.0"));
+        unitList.add(new Unit("km^3","Cubic Kilometer","1.E-9"));
+        unitList.add(new Unit("cm^3","Cubic Centimeter","1000000.0"));
+        unitList.add(new Unit("mm^3","Cubic Milimeter","1000000000.0"));
         unitList.add(new Unit("l","Liter","1000"));
-        unitList.add(new Unit("mi3","Cubic Mile","2.399128636E-10"));
-        unitList.add(new Unit("yd3","Cubic Yard","1.3079506193"));
-        unitList.add(new Unit("ft3","Cubic Foot","35.314666721"));
-        unitList.add(new Unit("in3","Cubic Inch","61023.744095"));
+        unitList.add(new Unit("mi^3","Cubic Mile","2.399128636E-10"));
+        unitList.add(new Unit("yd^3","Cubic Yard","1.3079506193"));
+        unitList.add(new Unit("ft^3","Cubic Foot","35.314666721"));
+        unitList.add(new Unit("in^3","Cubic Inch","61023.744095"));
         unitList.add(new Unit("gal","US Gallon","264.17217686s"));
         return unitList;
     }
